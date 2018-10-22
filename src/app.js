233160866d0651e0eb556540179fdf37195eace1
Eero Sihvonen <sihvonen.eero@gmail.com>
2018-10-22 17:13:34 +0300

    removing dispatch calls from app.js

--- src/app.js
+++ src/app.js
@@ -3,27 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'; 
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expences';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css'; 
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-
-store.subscribe(() => {
-    const state = store.getState();
-    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-    console.log(visibleExpenses);
-});
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 5500, createdAt: 124000}));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 21500, createdAt: 125000}));
-store.dispatch(addExpense({ description: 'Rent', amount: 10960, createdAt: 127000}));
-//store.dispatch(setTextFilter('bill'));
-//store.dispatch(setTextFilter('water'));
-console.log(store.getState());
 
 const jsx = (
     <Provider store={store}>
