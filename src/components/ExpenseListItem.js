13043a3594919772340f149293027e3471a564ce
Eero Sihvonen <sihvonen.eero@gmail.com>
2018-10-25 19:47:41 +0300

    numeral.js

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListitem = ({ description, id, amount, createdAt}) => (
 
     <div>
         <Link to={`/edit/${id}`}>{description}</Link>
-        <p>{amount}</p>
-        <p>{createdAt}</p>
+        <p>
+        { numeral(amount / 100).format('$0,0.00') }
+        -
+        { moment(createdAt).format('MMMM Do YYYY') }
+        </p>
     </div>
 );
 
